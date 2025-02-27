1630855037b03c9afeac5cc349ba08e3d66767fb
Maksym Kucherov <maximus.the0@gmail.com>
2025-02-27 18:58:31 +0200

    REFACT: Remove C++ projects from SLN. Use CMake.

--- FiftyOne.IpIntelligence/FiftyOne.IpIntelligence.Engine.OnPremise.Native/PreBuild.bat
+++
@@ -1,7 +0,0 @@
-@for %%X in (swig.exe) do (set SWIG_EXE=%%~$PATH:X)
-@if defined SWIG_EXE (
-@echo SWIG auto generated code being rebuilt.
-swig -c++ -csharp -namespace FiftyOne.IpIntelligence.Engine.OnPremise.Interop -dllimport FiftyOne.IpIntelligence.Engine.OnPremise.Native.dll -outdir ../FiftyOne.IpIntelligence.Engine.OnPremise/Interop/Swig -o ../IpIntelligenceEngineSwig_csharp.cpp ipi_csharp.i
-) else (
-@echo SWIG not found. SWIG auto generated code will not be rebuilt.
-)
--- FiftyOne.IpIntelligence/FiftyOne.IpIntelligence.Engine.OnPremise.Native/PreBuild.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if command -v swig >/dev/null 2>&1; then
-    { echo >&2 "Generating Swig wrapper for Hash."; }
-    swig -c++ -csharp -namespace FiftyOne.IpIntelligence.Engine.OnPremise.Interop -dllimport FiftyOne.IpIntelligence.Engine.OnPremise.Native.dll -outdir ../FiftyOne.IpIntelligence.Engine.OnPremise/Interop/Swig -o ../IpIntelligenceEngineSwig_csharp.cpp ipi_csharp.i
-else
-    { echo >&2 "Swig is required to generate wrapper but it's not installed."; }
-fi
