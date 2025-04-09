2f86689204718e09c3d9ae039076fd9d2c9b056a
Maksym Kucherov <maximus.the0@gmail.com>
2025-04-09 11:10:45 +0300

    CI: Add github as source for restore.

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -1,6 +1,7 @@
 param(
     [Parameter(Mandatory)][string]$RepoName,
     [Parameter(Mandatory)][string]$OrgName,
+    [string]$GitHubUser = "Automation51D",
     [Parameter(Mandatory)][string]$DeviceDetection,
     [Parameter(Mandatory)][string]$DeviceDetectionUrl,
     [string]$ProjectDir = ".",
@@ -35,6 +36,12 @@ try {
 } finally {
     Pop-Location
 }
+
+
+./dotnet/add-nuget-source.ps1 `
+    -Source "https://nuget.pkg.github.com/$OrgName/index.json" `
+    -UserName $GitHubUser `
+    -Key $env:GITHUB_TOKEN
 
 Push-Location $ExamplesRepo
 try {
