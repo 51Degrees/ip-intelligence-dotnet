7279628d5651fedaa739bb1deeb8b10b5594998a
Maksym Kucherov <maximus.the0@gmail.com>
2025-04-30 18:15:46 +0300

    CI: Apply `update-packages.ps1` from examples repo.

--- ci/update-packages.ps1
+++ ci/update-packages.ps1
@@ -1,11 +1,28 @@
-
+[CmdletBinding()]
 param(
+    [Parameter(Mandatory=$true)]
+    [string]$OrgName,
+    [string]$GitHubUser = "Automation51D",
     [string]$ProjectDir = ".",
     [string]$Name,
     [Parameter(Mandatory=$true)]
     [string]$RepoName
 )
 
-./dotnet/run-update-dependencies.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name
+./dotnet/add-nuget-source.ps1 `
+    -Source "https://nuget.pkg.github.com/$OrgName/index.json" `
+    -UserName $GitHubUser `
+    -Key $env:GITHUB_TOKEN
+
+if ($LASTEXITCODE -eq 0) {
+    ./dotnet/run-update-dependencies.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -Name $Name -IncludePrerelease
+}
+
+if ($LASTEXITCODE -ne 0) {
+    $ErrorActionPreference = "Stop"
+    $PSNativeCommandUseErrorActionPreference = $true
+
+    Write-Error "LASTEXITCODE = $LASTEXITCODE"
+}
 
 exit $LASTEXITCODE
