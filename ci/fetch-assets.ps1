97ca68b432eb07b96a27b8e5bdacfa32371a2d7c
Maksym Kucherov <maximus.the0@gmail.com>
2025-05-01 11:01:51 +0300

    CI: Delegate fetch-assets to cxx script. pt.3

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -6,8 +6,8 @@ param (
     [string]$DeviceDetectionUrl
 )
 
-$CxxCiDir = Join-Path $RepoName "FiftyOne.IpIntelligence.Engine.OnPremise" "ip-intelligence-cxx" "ci"
-$CxxCiScript = Join-Path $pwd $CxxCiDir "fetch-assets.ps1"
+$CxxCiDir = Join-Path $RepoName "FiftyOne.IpIntelligence.Engine.OnPremise" "ip-intelligence-cxx"
+$CxxCiScript = Join-Path $pwd $CxxCiDir "ci" "fetch-assets.ps1"
 
 & $CxxCiScript `
     -RepoName $CxxCiDir `
