cffc471c77f2078fa4f810d01d95b0de9a61df50
Maksym Kucherov <maximus.the0@gmail.com>
2025-05-02 17:38:20 +0300

    FEAT: Copy `performance-tests` from device-detection.

---
+++ performance-tests/Controllers/ProcessController.cs
@@ -0,0 +1,54 @@
+/* *********************************************************************
+ * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2023 51 Degrees Mobile Experts Limited, Davidson House,
+ * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
+ *
+ * This Original Work is licensed under the European Union Public Licence
+ * (EUPL) v.1.2 and is subject to its terms as set out below.
+ *
+ * If a copy of the EUPL was not distributed with this file, You can obtain
+ * one at https://opensource.org/licenses/EUPL-1.2.
+ *
+ * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+ * amended by the European Commission) shall be deemed incompatible for
+ * the purposes of the Work and the provisions of the compatibility
+ * clause in Article 5 of the EUPL shall not apply.
+ *
+ * If using the Work as, or as part of, a network application, by
+ * including the attribution notice(s) required under Article 5 of the EUPL
+ * in the end user terms of the application under an appropriate heading,
+ * such notice(s) shall fulfill the requirements of that article.
+ * ********************************************************************* */
+
+using Microsoft.AspNetCore.Mvc;
+using FiftyOne.Pipeline.Web.Services;
+using FiftyOne.DeviceDetection;
+
+namespace performance_tests.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcessController : ControllerBase
+    {
+        private IFlowDataProvider _flow;
+
+        public ProcessController(IFlowDataProvider flow)
+        {
+            _flow = flow;
+        }
+
+        [HttpGet]
+        public string Get(){
+            var device = _flow.GetFlowData()?.Get<IDeviceData>();
+            if(device != null) {
+                if (device.IsMobile.HasValue) {
+                    return $"{device.IsMobile}";
+                }
+                else {
+                    return $"{device.IsMobile.NoValueMessage}";
+                }
+            }
+            return "Hash engine data was null";
+        }
+    }
+}
