dededb76342950e39df23f9110f75114395ffa85
Automation51D <51DCI@51Degrees.com>
2025-05-06 00:34:12 +0000

    Update properties

--- FiftyOne.IpIntelligence.Shared/Data/IIpIntelligenceData.cs
+++ FiftyOne.IpIntelligence.Shared/Data/IIpIntelligenceData.cs
@@ -1,6 +1,6 @@
 /* *********************************************************************
  * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
- * Copyright 2025 51 Degrees Mobile Experts Limited, Davidson House,
+ * Copyright 2023 51 Degrees Mobile Experts Limited, Davidson House,
  * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
  *
  * This Original Work is licensed under the European Union Public Licence
@@ -13,10 +13,10 @@
  * amended by the European Commission) shall be deemed incompatible for
  * the purposes of the Work and the provisions of the compatibility
  * clause in Article 5 of the EUPL shall not apply.
- *
- * If using the Work as, or as part of, a network application, by
+ * 
+ * If using the Work as, or as part of, a network application, by 
  * including the attribution notice(s) required under Article 5 of the EUPL
- * in the end user terms of the application under an appropriate heading,
+ * in the end user terms of the application under an appropriate heading, 
  * such notice(s) shall fulfill the requirements of that article.
  * ********************************************************************* */
 using System.Net;
@@ -35,50 +35,6 @@ namespace FiftyOne.IpIntelligence
 	public interface IIpIntelligenceData : IAspectData
 	{
 		/// <summary>
-		/// Accuracy radius of the matched location in meters.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<int>>> AccuracyRadius { get; }
-		/// <summary>
-		/// Any shapes associated with the location. Usually this is the area which the IP range covers.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Areas { get; }
-		/// <summary>
-		/// The name of the country that the supplied location is in.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Country { get; }
-		/// <summary>
-		/// The 2-character ISO 3166-1 code of the country that the supplied location is in.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> CountryCode { get; }
-		/// <summary>
-		/// The 3-character ISO 3166-1 alpha-3 code of the country that the supplied location is in.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> CountryCode3 { get; }
-		/// <summary>
-		/// End of the IP range to which the evidence IP belongs.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<IPAddress>>> IpRangeEnd { get; }
-		/// <summary>
-		/// Start of the IP range to which the evidence IP belongs.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<IPAddress>>> IpRangeStart { get; }
-		/// <summary>
-		/// Average latitude of the IP. For privacy, this is randomized within around 1 mile of the result. Randomized result will change only once per day.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<float>>> Latitude { get; }
-		/// <summary>
-		/// Average longitude of the IP. For privacy, this is randomized within around 1 mile of the result. Randomized result will change only once per day.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<float>>> Longitude { get; }
-		/// <summary>
-		/// The mobile country code of the network the device is connected to.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Mcc { get; }
-		/// <summary>
-		/// The name of the geographical region that the supplied location is in.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Region { get; }
-		/// <summary>
 		/// Country code of the registered range.
 		/// </summary>
 		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> RegisteredCountry { get; }
@@ -90,17 +46,5 @@ namespace FiftyOne.IpIntelligence
 		/// Registered owner of the range.
 		/// </summary>
 		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> RegisteredOwner { get; }
-		/// <summary>
-		/// The name of the state that the supplied location in in.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> State { get; }
-		/// <summary>
-		/// The offset from UTC in minutes in the supplied location, at the time that the value is produced.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<int>>> TimeZoneOffset { get; }
-		/// <summary>
-		/// The name of the town that the supplied location is in.
-		/// </summary>
-		IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Town { get; }
 	}
 }
--- FiftyOne.IpIntelligence.Shared/IpIntelligenceDataBase.cs
+++ FiftyOne.IpIntelligence.Shared/IpIntelligenceDataBase.cs
@@ -1,6 +1,6 @@
 /* *********************************************************************
  * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
- * Copyright 2025 51 Degrees Mobile Experts Limited, Davidson House,
+ * Copyright 2023 51 Degrees Mobile Experts Limited, Davidson House,
  * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
  *
  * This Original Work is licensed under the European Union Public Licence
@@ -13,10 +13,10 @@
  * amended by the European Commission) shall be deemed incompatible for
  * the purposes of the Work and the provisions of the compatibility
  * clause in Article 5 of the EUPL shall not apply.
- *
- * If using the Work as, or as part of, a network application, by
+ * 
+ * If using the Work as, or as part of, a network application, by 
  * including the attribution notice(s) required under Article 5 of the EUPL
- * in the end user terms of the application under an appropriate heading,
+ * in the end user terms of the application under an appropriate heading, 
  * such notice(s) shall fulfill the requirements of that article.
  * ********************************************************************* */
 using System.Net;
@@ -66,33 +66,11 @@ namespace FiftyOne.IpIntelligence.Shared
 		protected static readonly IReadOnlyDictionary<string, Type> PropertyTypes =
 			new Dictionary<string, Type>()
 			{
-				{ "AccuracyRadius", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<int>>>) },
-				{ "Areas", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
-				{ "Country", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
-				{ "CountryCode", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
-				{ "CountryCode3", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
-				{ "IpRangeEnd", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<IPAddress>>>) },
-				{ "IpRangeStart", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<IPAddress>>>) },
-				{ "Latitude", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<float>>>) },
-				{ "Longitude", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<float>>>) },
-				{ "Mcc", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
-				{ "Region", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
 				{ "RegisteredCountry", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
 				{ "RegisteredName", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
-				{ "RegisteredOwner", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
-				{ "State", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) },
-				{ "TimeZoneOffset", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<int>>>) },
-				{ "Town", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) }
+				{ "RegisteredOwner", typeof(IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>) }
 			};
 
-		/// <summary>
-		/// End of the IP range to which the evidence IP belongs.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<IPAddress>>> IpRangeEnd { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<IPAddress>>>>("IpRangeEnd"); } }
-		/// <summary>
-		/// Start of the IP range to which the evidence IP belongs.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<IPAddress>>> IpRangeStart { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<IPAddress>>>>("IpRangeStart"); } }
 		/// <summary>
 		/// Country code of the registered range.
 		/// </summary>
@@ -105,53 +83,5 @@ namespace FiftyOne.IpIntelligence.Shared
 		/// Registered owner of the range.
 		/// </summary>
 		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> RegisteredOwner { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>>("RegisteredOwner"); } }
-		/// <summary>
-		/// Accuracy radius of the matched location in meters.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<int>>> AccuracyRadius { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<int>>>>("AccuracyRadius"); } }
-		/// <summary>
-		/// Any shapes associated with the location. Usually this is the area which the IP range covers.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Areas { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>>("Areas"); } }
-		/// <summary>
-		/// The name of the country that the supplied location is in.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Country { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>>("Country"); } }
-		/// <summary>
-		/// The 2-character ISO 3166-1 code of the country that the supplied location is in.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> CountryCode { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>>("CountryCode"); } }
-		/// <summary>
-		/// The 3-character ISO 3166-1 alpha-3 code of the country that the supplied location is in.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> CountryCode3 { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>>("CountryCode3"); } }
-		/// <summary>
-		/// Average latitude of the IP. For privacy, this is randomized within around 1 mile of the result. Randomized result will change only once per day.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<float>>> Latitude { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<float>>>>("Latitude"); } }
-		/// <summary>
-		/// Average longitude of the IP. For privacy, this is randomized within around 1 mile of the result. Randomized result will change only once per day.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<float>>> Longitude { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<float>>>>("Longitude"); } }
-		/// <summary>
-		/// The name of the geographical region that the supplied location is in.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Region { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>>("Region"); } }
-		/// <summary>
-		/// The name of the state that the supplied location in in.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> State { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>>("State"); } }
-		/// <summary>
-		/// The offset from UTC in minutes in the supplied location, at the time that the value is produced.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<int>>> TimeZoneOffset { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<int>>>>("TimeZoneOffset"); } }
-		/// <summary>
-		/// The name of the town that the supplied location is in.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Town { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>>("Town"); } }
-		/// <summary>
-		/// The mobile country code of the network the device is connected to.
-		/// </summary>
-		public IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>> Mcc { get { return GetAs<IAspectPropertyValue<IReadOnlyList<IWeightedValue<string>>>>("Mcc"); } }
 	}
 }
