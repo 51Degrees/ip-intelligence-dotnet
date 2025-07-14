9d5c6873d5111f25dd6fd892ebd45bf5cd789b94
Maksym Kucherov <maximus.the0@gmail.com>
2025-07-14 12:46:37 +0300

    REFACT: Regen SWIG with 4.3.1. pt.2

--- FiftyOne.IpIntelligence.Engine.OnPremise/Interop/Swig/ValueMetaDataKeySwig.cs
+++ FiftyOne.IpIntelligence.Engine.OnPremise/Interop/Swig/ValueMetaDataKeySwig.cs
@@ -57,11 +57,7 @@ internal class ValueMetaDataKeySwig : global::System.IDisposable {
     }
   }
 
-  public ValueMetaDataKeySwig(string propertyName, string valueName) : this(IpIntelligenceEngineModulePINVOKE.new_ValueMetaDataKeySwig__SWIG_0(propertyName, valueName), true) {
-    if (IpIntelligenceEngineModulePINVOKE.SWIGPendingException.Pending) throw IpIntelligenceEngineModulePINVOKE.SWIGPendingException.Retrieve();
-  }
-
-  public ValueMetaDataKeySwig(string propertyName, byte[] data, int length) : this(IpIntelligenceEngineModulePINVOKE.new_ValueMetaDataKeySwig__SWIG_1(propertyName, data, length), true) {
+  public ValueMetaDataKeySwig(string propertyName, string valueName) : this(IpIntelligenceEngineModulePINVOKE.new_ValueMetaDataKeySwig(propertyName, valueName), true) {
     if (IpIntelligenceEngineModulePINVOKE.SWIGPendingException.Pending) throw IpIntelligenceEngineModulePINVOKE.SWIGPendingException.Retrieve();
   }
 
