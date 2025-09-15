78b742ba3943de81fd7dd33771b2f4fc68d9f33a
Maksym Kucherov <maximus.the0@gmail.com>
2025-09-15 14:20:33 +0300

    CI: Move files before performance tests.

--- ci/run-performance-tests-console.ps1
+++ ci/run-performance-tests-console.ps1
@@ -39,11 +39,11 @@ if ($(Test-Path -Path $ExamplesRepoName) -eq $False) {
 
 Write-Output "Moving enterprise IPI file"
 $EnterpriseFile = [IO.Path]::Combine($EvidenceFiles, "51Degrees-EnterpriseIpiV41.ipi") 
-Copy-Item $EnterpriseFile "ip-intelligence-dotnet-examples/ip-intelligence-data/51Degrees-EnterpriseIpiV41.ipi"
+Move-Item $EnterpriseFile "ip-intelligence-dotnet-examples/ip-intelligence-data/51Degrees-EnterpriseIpiV41.ipi"
 
 Write-Output "Moving evidence file"
 $EvidenceFile = [IO.Path]::Combine($EvidenceFiles, "evidence.yml")
-Copy-Item $EvidenceFile "ip-intelligence-dotnet-examples/ip-intelligence-data/evidence.yml"
+Move-Item $EvidenceFile "ip-intelligence-dotnet-examples/ip-intelligence-data/evidence.yml"
 
 
 function Edit-ExamplesCsprojRef {
