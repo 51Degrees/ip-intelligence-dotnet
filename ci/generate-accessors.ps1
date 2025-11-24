7c2fa21c189f08173e609c70af8fb0cc92cb73f9
Alexander Popel <alexander.popel@postindustria.com>
2025-11-24 12:11:53 +0200

    ci: update generate-accessors parameters

--- ci/generate-accessors.ps1
+++ ci/generate-accessors.ps1
@@ -1,10 +1,11 @@
 param (
-    [Parameter(Mandatory)][string]$RepoName
+    [Parameter(Mandatory)][string]$RepoName,
+    [string]$MetaDataPath = "$PWD/common-metadata",
+    [string]$DataType = "IpIntelligence"
 )
 $ErrorActionPreference = "Stop"
-$PSNativeCommandUseErrorActionPreference = $true
 
-./tools/ci/generate-accessors.ps1 @PSBoundParameters
+./tools/ci/generate-accessors.ps1 -RepoName:$RepoName -MetaDataPath:$MetaDataPath -DataType:$DataType
 
 Copy-Item "tools/CSharp/IIpIntelligenceData.cs" "ip-intelligence-dotnet/FiftyOne.IpIntelligence.Shared/Data/"
 Copy-Item "tools/CSharp/IpIntelligenceDataBase.cs" "ip-intelligence-dotnet/FiftyOne.IpIntelligence.Shared/"
