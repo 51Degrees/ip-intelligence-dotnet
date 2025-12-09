36d10f3046cea9310297e1a42257571cc187421d
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2025-12-09 11:20:55 +0200

    Merged Pull Request '#171 fix-web-perf-tests->main: web perf tests: switch to a custom httpbench script'

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -1,37 +1,13 @@
-[CmdletBinding()]
 param(
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName,
+    [Parameter(Mandatory)][string]$RepoName,
     [string]$OrgName,
-    [string]$ProjectDir = ".",
     [string]$Name = "Release_x64",
     [string]$Configuration = "Release",
     [string]$Arch = "x64"
 )
 
-Write-Output "Running console performance tests..."
-& ./$RepoName/ci/run-performance-tests-console.ps1 `
-    -Debug `
-    -RepoName $RepoName `
-    -OrgName $OrgName `
-    -Name $Name `
-    -Configuration $Configuration `
-    -Arch $Arch
-if ($LASTEXITCODE -ne 0) {
-    Write-Warning "LASTEXITCODE = $LASTEXITCODE"
-    exit $LASTEXITCODE
-}
+Write-Host "Running console performance tests..."
+& $PSScriptRoot/run-performance-tests-console.ps1 -Debug -RepoName:$RepoName -OrgName:$OrgName -Name:$Name -Configuration:$Configuration -Arch:$Arch
 
-Write-Output "Running web performance tests..."
-& ./$RepoName/ci/run-performance-tests-web.ps1 `
-    -Debug `
-    -RepoName $RepoName `
-    -ProjectDir $ProjectDir `
-    -Name $Name `
-    -Configuration $Configuration `
-    -Arch $Arch
-if ($LASTEXITCODE -ne 0) {
-    Write-Warning "LASTEXITCODE = $LASTEXITCODE"
-}
-
-exit $LASTEXITCODE
+Write-Host "Running web performance tests..."
+& $RepoName/ci/run-performance-tests-web.ps1 -Name:$Name -Configuration:$Configuration -Arch:$Arch
