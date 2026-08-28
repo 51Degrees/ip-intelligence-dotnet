bab9b7f885a1590b78df723f6cc21a8d06821f91
Automation51D <60396166+Automation51D@users.noreply.github.com>
2026-08-28 06:58:24 +0200

    Merged Pull Request '#339 properties-update/main->main: Update properties'
    Co-authored-by: Automation51D <51DCI@51Degrees.com>

--- FiftyOne.IpIntelligence.Shared/Data/IIpIntelligenceData.cs
+++ FiftyOne.IpIntelligence.Shared/Data/IIpIntelligenceData.cs
@@ -45,14 +45,29 @@ namespace FiftyOne.IpIntelligence
 		/// <summary>
 		/// Any shapes associated with the location. Usually this is the area which the IP range covers. This is returned as a WKT String stored as a reduced format of WKB.
 		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// See <see href="https://en.wikipedia.org/wiki/Well-known_text_representation_of_geometry">https://en.wikipedia.org/wiki/Well-known_text_representation_of_geometry</see> for more information.
+		/// </para>
+		/// </remarks>
 		IAspectPropertyValue<WktString> Areas { get; }
 		/// <summary>
 		/// Autonomous System Number associated with the IP address.
 		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// See <see href="https://en.wikipedia.org/wiki/Autonomous_system_(Internet)">https://en.wikipedia.org/wiki/Autonomous_system_(Internet)</see> for more information.
+		/// </para>
+		/// </remarks>
 		IAspectPropertyValue<string> Asn { get; }
 		/// <summary>
 		/// The name registered to the Asn associated with the IP address.
 		/// </summary>
+		/// <remarks>
+		/// <para>
+		/// See <see href="https://en.wikipedia.org/wiki/Autonomous_system_(Internet)">https://en.wikipedia.org/wiki/Autonomous_system_(Internet)</see> for more information.
+		/// </para>
+		/// </remarks>
 		IAspectPropertyValue<string> AsnName { get; }
 		/// <summary>
 		/// Refers to the diversity of browsers observed from the IP range. An integer between 1-10, a lower value indicates a low number of browsers seen per IP address, while a higher value indicates a high number of browsers seen per IP address. A 0 value indicates that the probability is unknown.
